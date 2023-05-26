edf61a6907c6ec19f73e76c42dd93bc676f3d604
shai-mermelstein <shai.mermelstein@gmail.com>
2023-05-26 21:47:11 +0300

    m

--- Semantics_.v
+++ Semantics_.v
@@ -53,10 +53,10 @@ Inductive BSemantics : bexp -> bool -> pairsP state :=
       |= [|a1 = a2 ->b (n1 =? n2)%nat|]
   | SmLe : forall a1 a2 n1 n2,
     LP([|a1 -> n1|] ; [|a2 -> n2|]) 
-      |= [|a1 = a2 ->b (n1 <=? n2)%nat|]
-  | SmAndTrue : forall b1 b2,
+      |= [|a1 <= a2 ->b (n1 <=? n2)%nat|]
+  | SmAndFalse : forall b1 b2,
     [|b1|]f |= [|b1 && b2|]f
-  | SmAndFalse : forall b1 b2 v,
+  | SmAndTrue : forall b1 b2 v,
     LP([|b1|]t ; [|b2 ->b v|]) 
       |= [|b1 && b2 ->b v|]
   where "[| b |]t" := (BSemantics b true)
@@ -76,7 +76,7 @@ Inductive CSemantics : com -> pairsP state :=
   | SmIfTrue : forall b c1 c2,
     LP([|b|]t ; [|c1|]) 
       |= [|if b then c1 else c2 end|]
-  | SmIfFasle : forall b c1 c2,
+  | SmIfFalse : forall b c1 c2,
     LP([|b|]f ; [|c2|])
       |= [|if b then c1 else c2 end|]
   | SmWhile : forall b c,
@@ -122,9 +122,9 @@ Reserved Notation "[| b |]'f"
 Reserved Notation "[| b ->b v |]'"
   (at level 0, b custom com at level 99).
 Inductive BSemantics' : bexp -> bool -> pairsP state :=
-  | SmTrue' : forall s,
-    LP({[(s, s)]}) |= [|true|]'t
   | SmFalse' : forall s,
+    LP({[(s, s)]}) |= [|true|]'t
+  | SmTrue' : forall s,
     LP({[(s, s)]}) |= [|false|]'f
   | SmNot' : forall b v,
     [|b ->b v|]' |= [|~b ->b negb v|]'
@@ -133,10 +133,10 @@ Inductive BSemantics' : bexp -> bool -> pairsP state :=
       |= [|a1 = a2 ->b (n1 =? n2)%nat|]'
   | SmLe' : forall a1 a2 n1 n2,
     LP([|a1 -> n1|]' + [|a2 -> n2|]') 
-      |= [|a1 = a2 ->b (n1 <=? n2)%nat|]'
-  | SmAndTrue' : forall b1 b2,
+      |= [|a1 <= a2 ->b (n1 <=? n2)%nat|]'
+  | SmAndFalse' : forall b1 b2,
     [|b1|]'f |= [|b1 && b2|]'f
-  | SmAndFalse' : forall b1 b2 v,
+  | SmAndTrue' : forall b1 b2 v,
     LP([|b1|]'t + [|b2 ->b v|]') 
       |= [|b1 && b2 ->b v|]'
   where "[| b |]'t" := (BSemantics' b true)
@@ -156,7 +156,7 @@ Inductive CSemantics' : com -> pairsP state :=
   | SmIfTrue' : forall b c1 c2,
     LP([|b|]'t + [|c1|]') 
       |= [|if b then c1 else c2 end|]'
-  | SmIfFasle' : forall b c1 c2,
+  | SmIfFalse' : forall b c1 c2,
     LP([|b|]'f + [|c2|]')
       |= [|if b then c1 else c2 end|]'
   | SmWhile' : forall b c,
@@ -446,8 +446,370 @@ Proof with ellipsis.
     + invert IHstutter_mumble_closure.
       apply SmMult...
 Qed.
-  
-Theorem BSemantics_equiv' :
-  forall a v,
-    [|a ->b v|] ~ LP([|a ->b v|]'^).
+
+Lemma BSemantics_stuttery_mumbly :
+  forall b v,
+    stuttery_mumbly [|b ->b v|].
 Proof with ellipsis.
+  intro. induction b; repeat intro.
+  - remember [|true ->b v|] as P.
+    clean_induction H;
+      invert IHstutter_mumble_closure;
+      eapply SmTrue...
+  - remember [|false ->b v|] as P.
+    clean_induction H;
+      invert IHstutter_mumble_closure;
+      eapply SmFalse...
+  - remember [|a1 = a2 ->b v|] as P.
+    clean_induction H;
+      invert IHstutter_mumble_closure;
+      eapply SmEq...
+  - remember [|a1 <= a2 ->b v|] as P.
+    clean_induction H;
+      invert IHstutter_mumble_closure;
+      eapply SmLe...
+  - remember [|~ b ->b v|] as P.
+    clean_induction H.
+    + invert IHstutter_mumble_closure.
+      apply sm_self in H1.
+      apply sm_stuttery 
+        with (x:=x) in H1.
+      clean_apply_in IHb H1.
+      eapply SmNot...
+    + invert IHstutter_mumble_closure.
+      apply sm_self in H1.
+      apply sm_mumbly in H1.
+      clean_apply_in IHb H1.
+      eapply SmNot...
+  - remember [|b1 && b2 ->b v|] as P.
+    clean_induction H.
+    + invert IHstutter_mumble_closure.
+      * apply sm_self in H4.
+        apply sm_stuttery 
+          with (x:=x) in H4.
+        clean_apply_in IHb1 H4.
+        eapply SmAndFalse...
+      * apply SmAndTrue.
+        apply sm_stuttery...
+    + invert IHstutter_mumble_closure.
+      * apply sm_self in H4.
+        apply sm_mumbly in H4.
+        clean_apply_in IHb1 H4.
+        eapply SmAndFalse...
+      * apply SmAndTrue.
+        apply sm_mumbly with y...
+Qed.
+
+Theorem BSemantics_equiv' :
+  forall b v,
+    [|b ->b v|] ~ LP([|b ->b v|]'^).
+Proof with ellipsis.
+  intros; split; intros;
+    rename a into ts;
+    generalize dependent ts;
+    generalize dependent v;
+    clean_induction b...
+  - invert H.
+    remember LP({[(s, s)]}) as P.
+    clean_induction H0.
+    apply sm_self. eapply SmFalse'...
+  - invert H.
+    remember LP({[(s, s)]}) as P.
+    clean_induction H0.
+    apply sm_self. eapply SmTrue'...
+  - invert H.
+    remember LP([|a1 -> n1|] + [|a2 -> n2|]) 
+      as P.
+    clean_induction H4.
+    invert H.
+    destruct H0 as [y [H []]].
+    apply ASemantics_equiv' in H.
+    apply ASemantics_equiv' in H0.
+    remember LP([|a1 -> n1|]') as P.
+    generalize dependent l.
+    clean_induction H.
+    remember LP([|a2 -> n2|]') as P.
+    clean_induction H0.
+    + apply sm_self. apply SmEq'...
+    + rewrite app_assoc 
+        in IHstutter_mumble_closure.
+      apply sm_stuttery with (x:=x)
+        in IHstutter_mumble_closure.
+      rewrite app_assoc...
+    + rewrite app_assoc 
+        in IHstutter_mumble_closure.
+      apply sm_mumbly
+        in IHstutter_mumble_closure.
+      rewrite app_assoc...
+    + assert (
+        (l1 ++ l2) ++ y = (l1 ++ l2) ++ y
+      )...
+      clean_apply_in
+        IHstutter_mumble_closure H1.
+      rewrite <- app_assoc in H1.
+      apply sm_stuttery with (x:=x) in H1.
+      rewrite <- app_assoc...
+    + assert (
+        (l1 ++ [(x, y0); (y0, z)] ++ l2) ++ y
+          = 
+        (l1 ++ [(x, y0); (y0, z)] ++ l2) ++ y
+      )...
+      clean_apply_in
+        IHstutter_mumble_closure H1.
+      repeat rewrite <- app_assoc in H1.
+      apply sm_mumbly in H1.
+      rewrite <- app_assoc...
+  - invert H.
+    remember LP([|a1 -> n1|] + [|a2 -> n2|]) 
+      as P.
+    clean_induction H4.
+    invert H.
+    destruct H0 as [y [H []]].
+    apply ASemantics_equiv' in H.
+    apply ASemantics_equiv' in H0.
+    remember LP([|a1 -> n1|]') as P.
+    generalize dependent l.
+    clean_induction H.
+    remember LP([|a2 -> n2|]') as P.
+    clean_induction H0.
+    + apply sm_self. apply SmLe'...
+    + rewrite app_assoc 
+        in IHstutter_mumble_closure.
+      apply sm_stuttery with (x:=x)
+        in IHstutter_mumble_closure.
+      rewrite app_assoc...
+    + rewrite app_assoc 
+        in IHstutter_mumble_closure.
+      apply sm_mumbly
+        in IHstutter_mumble_closure.
+      rewrite app_assoc...
+    + assert (
+        (l1 ++ l2) ++ y = (l1 ++ l2) ++ y
+      )...
+      clean_apply_in
+        IHstutter_mumble_closure H1.
+      rewrite <- app_assoc in H1.
+      apply sm_stuttery with (x:=x) in H1.
+      rewrite <- app_assoc...
+    + assert (
+        (l1 ++ [(x, y0); (y0, z)] ++ l2) ++ y
+          = 
+        (l1 ++ [(x, y0); (y0, z)] ++ l2) ++ y
+      )...
+      clean_apply_in
+        IHstutter_mumble_closure H1.
+      repeat rewrite <- app_assoc in H1.
+      apply sm_mumbly in H1.
+      rewrite <- app_assoc...
+  - invert H.
+    clean_apply_in IHb H1.
+    remember [|b ->b v0 |]' as P.
+    clean_induction H1.
+    apply sm_self. apply SmNot'...
+  - invert H.
+    + clean_apply_in IHb1 H4.
+      clear IHb2.
+      remember [|b1 |]'f as P.
+      clean_induction H4.
+      apply sm_self. apply SmAndFalse'...
+    + remember LP([|b1 |]t + [|b2 ->b v|]) 
+        as P.
+      clean_induction H4.
+      invert H.
+      destruct H0 as [y [H []]].
+      clean_apply_in IHb1 H.
+      clean_apply_in IHb2 H0.
+      remember [|b1|]'t as P.
+      generalize dependent l.
+      clean_induction H.
+      remember [|b2|]'t as P.
+      clean_induction H0.
+      * apply sm_self. apply SmAndTrue'...
+      * rewrite app_assoc.
+        apply sm_stuttery.
+        rewrite <- app_assoc...
+      * rewrite app_assoc.
+        apply sm_mumbly with y.
+        rewrite <- app_assoc...
+      * repeat rewrite <- app_assoc.
+        apply sm_stuttery.
+        rewrite app_assoc.
+        apply IHstutter_mumble_closure...
+      * repeat rewrite <- app_assoc.
+        apply sm_mumbly with y0.
+        apply IHstutter_mumble_closure...
+        repeat rewrite app_assoc...
+  - remember [|true ->b v |]' as P.
+    clean_induction H.
+    + invert H. eapply SmTrue...
+    + invert IHstutter_mumble_closure.
+      eapply SmTrue...
+    + invert IHstutter_mumble_closure.
+      eapply SmTrue...
+  - remember [|false ->b v |]' as P.
+    clean_induction H.
+    + invert H. eapply SmFalse...
+    + invert IHstutter_mumble_closure.
+      eapply SmFalse...
+    + invert IHstutter_mumble_closure.
+      eapply SmFalse...
+  - remember [|a1 = a2 ->b v |]' as P.
+    clean_induction H.
+    + invert H. invert H4.
+      destruct H as [y [H []]].
+      apply sm_self in H.
+      apply ASemantics_equiv' in H.
+      apply sm_self in H0.
+      apply ASemantics_equiv' in H0.
+      eapply SmEq. apply sm_self...
+    + invert IHstutter_mumble_closure.
+      apply SmEq...
+    + invert IHstutter_mumble_closure.
+      apply SmEq...
+  - remember [|a1 = a2 ->b v |]' as P.
+    clean_induction H.
+    + invert H. invert H4.
+      destruct H as [y [H []]].
+      apply sm_self in H.
+      apply ASemantics_equiv' in H.
+      apply sm_self in H0.
+      apply ASemantics_equiv' in H0.
+      eapply SmLe. apply sm_self...
+    + invert IHstutter_mumble_closure.
+      apply SmLe...
+    + invert IHstutter_mumble_closure.
+      apply SmLe...
+  - remember [|~ b ->b v |]' as P.
+    clean_induction H.
+    + invert H. 
+      apply sm_self in H1.
+      clean_apply_in IHb H1.
+      apply SmNot...
+    + invert IHstutter_mumble_closure.
+      apply SmNot.
+      apply BSemantics_stuttery_mumbly...
+    + invert IHstutter_mumble_closure.
+      apply SmNot. 
+      apply BSemantics_stuttery_mumbly...
+  - remember [|b1 && b2 ->b v |]' as P.
+    clean_induction H.
+    + invert H.
+      * apply sm_self in H4.
+        clean_apply_in IHb1 H4.
+        apply SmAndFalse...
+      * invert H4.
+        destruct H as [y [H []]].
+        apply sm_self in H.
+        clean_apply_in IHb1 H.
+        apply sm_self in H0.
+        clean_apply_in IHb2 H0.
+        apply SmAndTrue. apply sm_self...
+    + invert IHstutter_mumble_closure.
+      * apply SmAndFalse...
+        apply BSemantics_stuttery_mumbly...
+      * apply SmAndTrue...
+    + invert IHstutter_mumble_closure.
+      * apply SmAndFalse...
+        apply BSemantics_stuttery_mumbly...
+      * apply SmAndTrue...
+Qed.
+        
+Theorem CSemantics_equiv' :
+  forall c,
+    [|c|] ~ LP([|c|]'^).
+Proof with ellipsis.
+  intros; split; intros;
+    rename a into ts;
+    generalize dependent ts;
+    clean_induction c...
+  - invert H.
+    remember LP({[(s, s)]}) as P.
+    clean_induction H0.
+    apply sm_self. eapply SmSkip'...
+  - invert H.
+    remember LP([|a -> n|] + {[(s, i |-> n; s)]}) 
+      as P.
+    clean_induction H3.
+    invert H. destruct H0 as [y [H []]].
+    subst.
+    apply ASemantics_equiv' in H.
+    remember [|a -> n|]' as P.
+    clean_induction H.
+    + apply sm_self. eapply SmAsgn'.
+      exists l, y. split...
+    + repeat rewrite <- app_assoc.
+      apply sm_stuttery.
+      rewrite app_assoc...
+    + repeat rewrite <- app_assoc.
+      apply sm_mumbly with y0.
+      repeat rewrite <- app_assoc 
+        in IHstutter_mumble_closure...
+  - invert H.
+    remember LP([|c1|] + [|c2|]) as P.
+    clean_induction H3.
+    invert H. destruct H0 as [y [H []]].
+    clean_apply_in IHc1 H.
+    clean_apply_in IHc2 H0.
+    remember [|c1 |]' as P.
+    generalize dependent l.
+    clean_induction H.
+    remember [|c2 |]' as P.
+    clean_induction H0.
+    + apply sm_self. apply SmSeq'.
+      exists l, l0...
+    + rewrite app_assoc in *.
+      apply sm_stuttery...
+    + rewrite app_assoc in *.
+      apply sm_mumbly with y...
+    + repeat rewrite <- app_assoc in *.
+      apply sm_stuttery...
+    + repeat rewrite <- app_assoc in *.
+      apply sm_mumbly with y0...
+  - invert H.
+    + remember LP([|b|]t + [|c2|]) as P.
+      clean_induction H4.
+      invert H. destruct H0 as [y [H []]].
+      apply BSemantics_equiv' in H.
+      clean_apply_in IHc1 H0.
+      remember [|b|]'t as P.
+      generalize dependent l.
+      clean_induction H.
+      remember [|c1|]' as P.
+      clean_induction H0.
+      * apply sm_self. apply SmIfTrue'.
+        exists l, l0...
+      * rewrite app_assoc in *.
+        apply sm_stuttery...
+      * rewrite app_assoc in *.
+        apply sm_mumbly with y...
+      * repeat rewrite <- app_assoc in *.
+        apply sm_stuttery...
+      * repeat rewrite <- app_assoc in *.
+        apply sm_mumbly with y0...
+    + remember LP([|b|]t + [|c2|]) as P.
+      clean_induction H4.
+      invert H. destruct H0 as [y [H []]].
+      apply BSemantics_equiv' in H.
+      clean_apply_in IHc2 H0.
+      remember [|b|]'t as P.
+      generalize dependent l.
+      clean_induction H.
+      remember [|c2|]' as P.
+      clean_induction H0.
+      * apply sm_self. apply SmIfFalse'.
+        exists l, l0...
+      * rewrite app_assoc in *.
+        apply sm_stuttery...
+      * rewrite app_assoc in *.
+        apply sm_mumbly with y...
+      * repeat rewrite <- app_assoc in *.
+        apply sm_stuttery...
+      * repeat rewrite <- app_assoc in *.
+        apply sm_mumbly with y0...
+  - invert H.
+    remember LP(([|b|]t; [|c|]) * + [|b|]f)
+      as P.
+    clean_induction H3.
+    invert H. destruct H0 as [y [H []]].
+    apply BSemantics_equiv' in H0.
+    
